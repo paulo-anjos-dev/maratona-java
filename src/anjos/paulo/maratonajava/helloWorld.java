89944569c6a44f5771924d82b2a37e7b55f9f47e
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-06-24 18:19:37 -0300

    Update helloWorld.java

--- src/anjos/paulo/maratonajava/helloWorld.java
+++ src/anjos/paulo/maratonajava/helloWorld.java
@@ -2,6 +2,6 @@ package anjos.paulo.maratonajava;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Meu milésimo 'Hello World' e meu primeiro KA-ME-KA-ME-HAAAAA");
+        System.out.println("Meu milésimo 'Hello World' e meu primeiro 'KA-ME-KA-ME-HAAAAA'");
     }
 }
