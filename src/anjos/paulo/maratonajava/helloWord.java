5aae0f6fd5fde887bf063e81e38dea8e29a21908
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-06-24 18:16:11 -0300

    Modificando o helloWord

--- src/anjos/paulo/maratonajava/helloWord.java
+++ src/anjos/paulo/maratonajava/helloWord.java
@@ -1,4 +1,7 @@
 package anjos.paulo.maratonajava;
 
 public class helloWord {
+    public static void main(String[] args) {
+        System.out.println("Meu milésimo 'Hello Word' e meu primeiro KA-ME-KA-ME-HAAAAA");
+    }
 }
