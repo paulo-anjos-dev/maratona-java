4d7353e6ea293de4faa7f64de8dcd4f51ad088de
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-06-24 18:29:33 -0300

    Create tiposPrimitivos.java

---
+++ src/anjos/paulo/maratonajava/tiposPrimitivos.java
@@ -0,0 +1,25 @@
+package anjos.paulo.maratonajava;
+
+public class tiposPrimitivos {
+    public static void main(String[] args) {
+        /**
+         * Tabela dos tipos primitivos
+         * +-----------+----------------+--------------+-------------------------------------------------------------+
+         * | Tipo      | Tamanho (bits) | Valor Padrão | Descrição                                                   |
+         * +-----------+----------------+--------------+-------------------------------------------------------------+
+         * | byte      | 8              | 0            | Número inteiro de -128 a 127. Usado para economia de memória.|
+         * | short     | 16             | 0            | Número inteiro de -32.768 a 32.767. Pouco usado.             |
+         * | int       | 32             | 0            | Número inteiro de -2.147.483.648 a 2.147.483.647.            |
+         * |           |                |              | Mais usado para inteiros.                                    |
+         * | long      | 64             | 0L           | Número inteiro de -9 quintilhões a +9 quintilhões.           |
+         * |           |                |              | Usa-se L no final.                                           |
+         * | float     | 32             | 0.0f         | Número decimal (ponto flutuante) de precisão simples.        |
+         * |           |                |              | Usa-se f no final.                                           |
+         * | double    | 64             | 0.0d         | Número decimal de precisão dupla. Mais usado que float.      |
+         * | char      | 16             | '\u0000'     | Representa um único caractere Unicode (letras, símbolos, etc.)|
+         * | boolean   | 1 (teórico)    | false        | Valores lógicos: true ou false.                              |
+         * +-----------+----------------+--------------+-------------------------------------------------------------+
+         */
+
+    }
+}
