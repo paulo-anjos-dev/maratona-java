9cf5599b90a02b837c37a9d99af4c7a99c32ab02
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-06-24 18:18:26 -0300

    Corrigindo grafia

--- src/anjos/paulo/maratonajava/helloWord.java
+++
@@ -1,7 +0,0 @@
-package anjos.paulo.maratonajava;
-
-public class helloWord {
-    public static void main(String[] args) {
-        System.out.println("Meu milésimo 'Hello Word' e meu primeiro KA-ME-KA-ME-HAAAAA");
-    }
-}
---
+++ src/anjos/paulo/maratonajava/helloWorld.java
@@ -0,0 +1,7 @@
+package anjos.paulo.maratonajava;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Meu milésimo 'Hello World' e meu primeiro KA-ME-KA-ME-HAAAAA");
+    }
+}
