5cc5bebdc16c1f5b0244a4d0276c418d35360f79
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-06-25 14:58:03 -0300

    Finalizando resumo

--- src/anjos/paulo/maratonajava/tiposPrimitivos.java
+++ src/anjos/paulo/maratonajava/tiposPrimitivos.java
@@ -21,5 +21,35 @@ public class tiposPrimitivos {
          * +-----------+----------------+--------------+-------------------------------------------------------------+
          */
 
+        // byte  | 8 bits  | Número inteiro de -128 a 127. Usado para economia de memória.
+        byte valorByte = 127;
+        System.out.println(valorByte);
+
+        // short | 16 bits  | Número inteiro de -32.768 a 32.767. Pouco usado.
+        short valorShort = 32767;
+        System.out.println(valorShort);
+
+        // int   | 32 bits  | Número inteiro de -2.147.483.648 a 2.147.483.647.
+        int valorInteiro = 2147483647;
+        System.out.println(valorInteiro);
+
+        // long  | 64 bits  | Número inteiro de -9 quintilhões a +9 quintilhões.
+        long valorLong = 9000000000000000000L;
+
+        // float | 32 bits  | Número decimal (ponto flutuante) de precisão simples.
+        float valorFloat = 10.00F;
+        System.out.println(valorFloat);
+
+        // double | 64      | Número decimal de precisão dupla. Mais usado que float.
+        double valorDouble = 10.00;
+        System.out.println(valorDouble);
+
+        // char   | 16 bits | Representa um único caractere Unicode (letras, símbolos, etc.).
+        char valorChar = '\u2561';
+        System.out.println(valorChar);
+
+        // boolean | 1 bit (teórico)  | Valores lógicos: true ou false.
+        boolean valorBoolean = true;
+        System.out.println(valorBoolean);
     }
 }
