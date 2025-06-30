8f94cbbc8eab5622ef3b50c6362e1743da36b8db
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-06-30 16:55:37 -0300

    Create EstruturasCondicionais.java

--- src/anjos/paulo/maratonajava/EstruturaCondicionais.java
+++ src/anjos/paulo/maratonajava/EstruturaCondicionais.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+package anjos.paulo.maratonajava;
 
 public class EstruturaCondicionais {
+    public static void main(String[] arg){
+        float salario = 1000.00F;
+        if (salario >= 1000){
+            System.out.println("Dentro do if");
+        } else if (salario < 1000) {
+            System.out.println("Dentro do else if");
+        }
+        else {
+            System.out.println("Dentro do else");
+        }
+        //Operador ternário (condição ? verdadeiro : falso)
+        System.out.println(salario > 5000 ? "Pode doar" : "Não pode doar");
+    }
 }
