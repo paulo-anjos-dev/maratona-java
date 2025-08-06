75ae484f5230fb12c40a9cb2d4e7fab3c63532db
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-08-06 15:58:32 -0300

    Exercicio finalisado

--- src/anjos/paulo/maratonajava/EstruturasDeRepeticaoExercicio.java
+++ src/anjos/paulo/maratonajava/EstruturasDeRepeticaoExercicio.java
@@ -1,10 +1,11 @@
 package anjos.paulo.maratonajava;
 import java.util.Scanner;
 public class EstruturasDeRepeticaoExercicio{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean calculadora = true;
-        while (calculadora){
+        int quantidadeNumeros;
+        while (calculadora) {
 
             System.out.println("------------------Bem Vindo------------------");
             System.out.println("1 - Somar");
@@ -16,24 +17,79 @@ public class EstruturasDeRepeticaoExercicio{
 
             System.out.print("Digite a opção desejada: ");
             int opcao = scanner.nextInt();
-
-            switch (opcao){
+            float valor = 0;
+            switch (opcao) {
                 case 1:
                     System.out.print("Quantos números deseja somar: ");
-                    int quantidadeNumeros = scanner.nextInt();
-                    int valor = 0;
-                    for (int contador = 0; contador < quantidadeNumeros; contador++){
+                    quantidadeNumeros = scanner.nextInt();
+                    for (int contador = 0; contador < quantidadeNumeros; contador++) {
                         System.out.printf("Digite o valor %d: ", contador + 1);
-                        int numero = scanner.nextInt();
+                        float numero = scanner.nextInt();
                         valor += numero;
                     }
-                    System.out.printf("A soma dos valores informados é: %d\n", valor);
+                    System.out.printf("A soma dos valores informados é: %.2f.\n", valor);
                     break;
                 case 2:
+                    System.out.println("Quantos números deseja subtrair: ");
+                    quantidadeNumeros = scanner.nextInt();
+                    if (quantidadeNumeros > 0) {
+                        System.out.print("Digite o valor 1: ");
+                        valor = scanner.nextInt();
+                        for (int contador = 1; contador < quantidadeNumeros; contador++) {
+                            System.out.printf("Digite o valor %d: ", contador + 1);
+                            float numero = scanner.nextInt();
+                            valor -= numero;
+                        }
+                    }
+                    System.out.printf("A subtração dos valores informados é: %.2f.\n", valor);
                     break;
                 case 3:
+                    System.out.print("Quantos números deseja dividir: ");
+                    quantidadeNumeros = scanner.nextInt();
+                    double resultado;
+                    if (quantidadeNumeros > 1) {
+                        System.out.print("Digite o valor 1: ");
+                        resultado = scanner.nextInt();
+                        for (int contador = 1; contador < quantidadeNumeros; contador++) {
+                            System.out.printf("Digite o valor %d: ", contador + 1);
+                            float numero = scanner.nextInt();
+                            if (numero == 0) {
+                                System.out.println("ERROR: IMPOSSÍVEL DIVIDIR POR 0!");
+                                break;
+                            }else {
+                                resultado /= numero;
+                            }
+                            System.out.printf("A divisão dos valores informados é: %.2f.\n", resultado);
+                        }
+                    }else {
+                        System.out.println("ERROR: SÓ É POSSÍVEL DIVIDIR SE TIVER PELO MENOS DOIS VALORES!");
+                    }
                     break;
                 case 4:
+                    System.out.println("Quantos números deseja multiplicar: ");
+                    quantidadeNumeros = scanner.nextInt();
+                    if (quantidadeNumeros > 1) {
+                        System.out.print("Digite o valor 1: ");
+                        valor = scanner.nextInt();
+                        if (valor == 0){
+                            System.out.println("Qualquer valor multiplicado por 0, o resultado será 0");
+                        }else {
+                            for (int contador = 1; contador < quantidadeNumeros; contador++) {
+                                System.out.printf("Digite o valor %d: ", contador + 1);
+                                float numero = scanner.nextInt();
+                                if (numero == 0){
+                                    System.out.println("Qualquer valor multiplicado por 0, o resultado será 0");
+                                    break;
+                                }else {
+                                    valor *= numero;
+                                }
+                                System.out.printf("A multiplicação dos valores informados é: %.2f.\n", valor);
+                            }
+
+                        }
+                    }else{
+                        System.out.println("ERROR: SÓ É POSSÍVEL MULTIPLICAR SE TIVER PELO MENOS DOIS VALORES!");
+                    }
                     break;
                 case 5:
                     calculadora = false;
@@ -43,6 +99,5 @@ public class EstruturasDeRepeticaoExercicio{
                     break;
             }
         }
-
     }
 }
---
+++ src/anjos/paulo/maratonajava/test.java
@@ -0,0 +1,7 @@
+package anjos.paulo.maratonajava;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(10-50);
+    }
+}
